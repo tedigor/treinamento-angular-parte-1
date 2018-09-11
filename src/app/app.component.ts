bcd1815366c9fb44dabcd953f23e6b01a6a12c3f
Ted Medeiros <ted.medeiros@ctis.com.br>
2018-09-11 14:30:37 -0300

    aula 2 - criando o primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'É melhor Jair se acostumando!';
-  description='bolsomito';
-  url = 'http://cdn01.justificando.cartacapital.com.br/wp-content/uploads/2017/06/09155558/bolsonaro.png'
+  photos = [
+    {
+      url: 'http://cdn01.justificando.cartacapital.com.br/wp-content/uploads/2017/06/09155558/bolsonaro.png',
+      description: 'bolsomito'
+    },
+    {
+      url: 'https://cdn-images-1.medium.com/max/645/1*OqoJL4hclVsoQwBgqoZnHg.jpeg',
+      description: 'mito'
+    },
+    {
+      url: 'https://i2.wp.com/gigadicas.com/v2/wp-content/uploads/2017/08/bolsomito.jpg?fit=900%2C450',
+      description: 'mitoAmor'
+    }
+  ];
 }
